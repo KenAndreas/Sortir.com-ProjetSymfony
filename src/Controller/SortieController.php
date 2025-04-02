f5fb169a0e5aecf9551b98016575e408d3327106
Mélissa Cochet <78727080+Mallomee@users.noreply.github.com>
2025-04-02 09:52:52 +0200

    ajout route post filtre

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,23 +3,29 @@
 namespace App\Controller;
 
 use App\Entity\Campus;
-use App\Entity\Etat;
-use App\Entity\Lieu;
-use App\Entity\Participant;
 use App\Entity\Sortie;
 use Doctrine\ORM\EntityManagerInterface;
-use phpDocumentor\Reflection\DocBlock\Tags\Source;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class SortieController extends AbstractController
 {
-    #[Route('/sortie', name: 'app_sortie')]
+    #[Route('/', name: 'app_sortie', methods: ['GET'])]
     public function index(EntityManagerInterface $em): Response
     {
         return $this->render('sortie/index.html.twig', [
-            'controller_name' => 'SortieController',
+            'campus' => $em->getRepository(Campus::class)->findAll(),
+            'sorties' => $em->getRepository(Sortie::class)->findAll(),
+            'today' => new \DateTime(),
+        ]);
+    }
+
+    #[Route('/', name: 'app_sorties_filter', methods: ['POST'])]
+    public function getFiltre(Request $request, EntityManagerInterface $em): Response
+    {
+        return $this->render('sortie/index.html.twig', [
             'campus' => $em->getRepository(Campus::class)->findAll(),
             'sorties' => $em->getRepository(Sortie::class)->findAll(),
             'today' => new \DateTime(),
