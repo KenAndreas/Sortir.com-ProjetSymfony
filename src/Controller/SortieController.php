61c0511ac7b827fbfd0deadcdd7edca6c3ebf50d
KenAndreas <timor21@icloud.com>
2025-04-04 10:47:24 +0200

    Ajout page détail participant

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -141,4 +141,20 @@ final class SortieController extends AbstractController
             'form' => $form,
         ]);
     }
+
+
+    #[Route('/detail-profil/{id}', name: 'app_detail_participant')]
+    public function show(int $id, EntityManagerInterface $em): Response
+    {
+        // Récupérer l'entité Participant par ID
+        $participant = $em->getRepository(Participant::class)->find($id);
+
+        if (!$participant) {
+            throw $this->createNotFoundException('Participant non trouvé');
+        }
+
+        return $this->render('profile/detail.html.twig', [
+            'participant' => $participant,
+        ]);
+    }
 }
